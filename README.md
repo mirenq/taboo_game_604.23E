d49d17d77075838d327da96e9363ed536e2e326b
Ali <abbasovali935@gmail.com>
2025-05-05 15:49:43 +0400

    Create project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Group:
+- 604.23E
+
+# Students:
+- Ali Abbasov
+- Rasul Hacili
+- Rasul Aliyev
+- Mahdi Taebi
+
+# Project:
+- Taboo game
---
+++ android/app/src/main/kotlin/com/example/taboo_game_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taboo_game_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
